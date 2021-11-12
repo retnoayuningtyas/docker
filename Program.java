0e7374ace77904aff378373958b7bba0ea9ad3f4
retnoayuningtyas <64393657+retnoayuningtyas@users.noreply.github.com>
2021-11-12 18:29:55 +0700

    Update and rename program.java to Program.java

--- Program.java
+++ Program.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package program;
+package Program;
 import java.util.Scanner;
 /**
  *
  * @author 
  */
-public class program {
+public class Program {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
       
